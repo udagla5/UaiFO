f3edcc558eadea259cc287f38305d7377b0846f1
udagla5 <carloseduardopbernardo.ismart@gmail.com>
2026-06-13 00:49:22 -0300

    Sistema Vida player + prototipo Grid Torres

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,22 +4,32 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var invulneravel: bool = false
+@export var tempo_max_invuneravel: float
+var tempo_atual_invuneravel: float = 0
+
 @export var animation: AnimatedSprite2D
 @export var tower_scene: PackedScene
+
 
 func _ready() -> void:
 	pass
 
 func _physics_process(delta: float) -> void:
+	if invulneravel:
+		tempo_atual_invuneravel += delta
+		
+		if tempo_atual_invuneravel >= tempo_max_invuneravel:
+			invulneravel = false
+			tempo_atual_invuneravel = 0.0
+			animation.modulate = Color(1, 1, 1, 1)
+	
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = direction * SPEED
 
-	# 2. ANIMAÇÃO: Decidimos qual lado mostrar baseando-se no eixo mais forte
 	if direction != Vector2.ZERO:
 		
-		# Usamos >= para garantir que as diagonais perfeitas (teclado) não deem erro
 		if abs(direction.x) >= abs(direction.y):
-			# Movimento horizontal é mais forte ou igual (prioridade para os lados)
 			if direction.x > 0:
 				animation.play("walk_right")
 			else:
@@ -39,11 +49,30 @@ func _physics_process(delta: float) -> void:
 		place_tower()
 
 func place_tower() -> void:
+	var posicao_player = self.position
+	var posicao_torre_x: int = int(posicao_player.x/16)
+	var posicao_torre_y: int = int(posicao_player.y/16)
+	
+	print("Posição Player: ", posicao_player)
+	print("Posição Torre Relativa X: ", posicao_torre_x)
+	print("Posição Torre Relativa Y: ", posicao_torre_y)
 	if tower_scene != null:
 		var new_tower = tower_scene.instantiate()
 		if GameController.dinheiro_atual >= new_tower.stats.preco:
 			get_parent().add_child(new_tower)
-			new_tower.global_position = global_position
+			new_tower.global_position.x = (posicao_torre_x+1) * 16 - 8
+			new_tower.global_position.y = (posicao_torre_y+1) * 16 - 8
+			print("Posição Torre Reak X: ", new_tower.global_position.x)
+			print("Posição Torre Real Y: ", new_tower.global_position.y)
 			GameController.diminui_dinheiro(new_tower.stats.preco)
 		else:
 			new_tower.free()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("enemy") and not invulneravel:
+		animation.modulate = Color(1, 0, 0, 0.7)
+		animation.play("damage")
+		GameController.damage_player(1)
+		invulneravel = true
+		tempo_atual_invuneravel = 0
